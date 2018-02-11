c57364748b035460f41e2e162e30e1aa60cba014
micheled <mail@thisisdurante.com>
2018-02-11 16:38:08 +0100

    Imports sample code from other projects

---
+++ WindowsFormsApp1/StreamWriter.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace NiceMeter
+{
+    class StreamWriter : TextWriter
+    {
+        TextBox Output = null;
+
+        public StreamWriter(TextBox output)
+        {
+            Output = output;
+        }
+
+        public override void Write(char value)
+        {
+            base.Write(value);
+            Output.AppendText(value.ToString()); // When character data is written, append it to the text box.
+        }
+
+        public override Encoding Encoding
+        {
+            get { return Encoding.UTF8; }
+        }
+    }
+}
