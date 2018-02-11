3555b08c36e23b6ecd8a4b51bd2fb3c4261e491e
micheled <mail@thisisdurante.com>
2018-02-12 00:13:40 +0100

    Begins work on table view. Disables StreamWriter

--- NiceMeter/MainWindow.Designer.cs
+++ NiceMeter/MainWindow.Designer.cs
@@ -28,42 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.TextArea = new System.Windows.Forms.TextBox();
+            this.GridView = new System.Windows.Forms.DataGridView();
+            this.Hardware = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.HardwareValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.GridView)).BeginInit();
             this.SuspendLayout();
             // 
-            // TextArea
+            // gridView
             // 
-            this.TextArea.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.TextArea.Location = new System.Drawing.Point(13, 15);
-            this.TextArea.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.TextArea.Multiline = true;
-            this.TextArea.Name = "TextArea";
-            this.TextArea.Size = new System.Drawing.Size(304, 292);
-            this.TextArea.TabIndex = 0;
-            this.TextArea.TextChanged += new System.EventHandler(this.TextArea_TextChanged);
+            this.GridView.AllowUserToAddRows = false;
+            this.GridView.AllowUserToDeleteRows = false;
+            this.GridView.AllowUserToResizeColumns = false;
+            this.GridView.AllowUserToResizeRows = false;
+            this.GridView.CausesValidation = false;
+            this.GridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridView.ColumnHeadersVisible = false;
+            this.GridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Hardware,
+            this.HardwareValue});
+            this.GridView.EnableHeadersVisualStyles = false;
+            this.GridView.Location = new System.Drawing.Point(8, 8);
+            this.GridView.Name = "gridView";
+            this.GridView.RowHeadersVisible = false;
+            this.GridView.Size = new System.Drawing.Size(316, 464);
+            this.GridView.TabIndex = 1;
+            this.GridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Hardware
+            // 
+            this.Hardware.HeaderText = "Hardware";
+            this.Hardware.Name = "Hardware";
+            this.Hardware.ReadOnly = true;
+            // 
+            // HardwareValue
+            // 
+            this.HardwareValue.HeaderText = "HardwareValue";
+            this.HardwareValue.Name = "HardwareValue";
+            this.HardwareValue.ReadOnly = true;
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(332, 321);
+            this.ClientSize = new System.Drawing.Size(332, 481);
             this.ControlBox = false;
-            this.Controls.Add(this.TextArea);
+            this.Controls.Add(this.GridView);
             this.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "MainWindow";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "NiceMeter";
             this.Load += new System.EventHandler(this.MainWindow_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.GridView)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox TextArea;
+        private System.Windows.Forms.DataGridView GridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Hardware;
+        private System.Windows.Forms.DataGridViewTextBoxColumn HardwareValue;
     }
 }
 
--- NiceMeter/MainWindow.cs
+++ NiceMeter/MainWindow.cs
@@ -2,36 +2,62 @@
 using System.Collections.Generic;
 using System.Windows.Forms;
 using OpenHardwareMonitor.Hardware;
+using System.Linq;
 
 namespace NiceMeter
 {
     public partial class MainWindow : Form
     {
-        Computer Computer;
-        Timer Timer = new Timer { Enabled = true, Interval = 500 };
+        private Computer Computer = new Computer
+        {
+            MainboardEnabled = true,
+            CPUEnabled = true,
+            RAMEnabled = true,
+            GPUEnabled = true,
+            FanControllerEnabled = true,
+            HDDEnabled = true
+        };
+
+        private BindingSource BSource = new BindingSource();
+
+        private Timer Timer = new Timer
+        {
+            Enabled = true,
+            Interval = 500
+        };
 
         /// <summary>
         /// Timed function that returns the current values of the sensors
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        void TimerTick(object sender, EventArgs e)
+        private void TimerTick(object sender, EventArgs e)
         {
-            foreach (var hardwareItem in Computer.Hardware)
-            {
-                if (hardwareItem.HardwareType == HardwareType.CPU)
-                {
-                    hardwareItem.Update();
-                    foreach (IHardware subHardware in hardwareItem.SubHardware)
-                        subHardware.Update();
+            //// Clear previous text
+            //TextArea.Clear();
 
-                    foreach (var sensor in hardwareItem.Sensors)
-                    {
-                        if (sensor.SensorType == SensorType.Temperature)
-                        {
-                            Console.WriteLine(String.Format("{0} Temperature = {1}", sensor.Name, sensor.Value.HasValue ? sensor.Value.Value.ToString() : "no value"));
-                        }
-                    }
+            //// Gets a list of enabled hardware strings
+            //var HardwareList = Computer.Hardware.Select(x => x.HardwareType).ToList();
+
+            //foreach (var Hardware in Computer.Hardware)
+            //{
+            //    Hardware.Update();
+
+            //    foreach (var Sensor in Hardware.Sensors)
+            //    {
+            //        if (Sensor.Value.HasValue)
+            //            Console.WriteLine(String.Format("{0} - {1} - {2} = {3}", Hardware.HardwareType, Sensor.Name, Sensor.SensorType, Sensor.Value));
+            //    }
+            //}
+
+            foreach (var Hardware in Computer.Hardware)
+            {
+                Hardware.Update();
+
+                foreach (var Sensor in Hardware.Sensors)
+                {
+                    if (Sensor.Value.HasValue)
+                        BSource.Add(String.Format("{0} - {1} - {2} = {3}", Hardware.HardwareType, Sensor.Name, Sensor.SensorType, Sensor.Value));
                 }
             }
         }
@@ -53,7 +79,18 @@ namespace NiceMeter
         private void MainWindow_Load(object sender, EventArgs e)
         {
             // Redirect the out Console stream
-            Console.SetOut(new StreamWriter(TextArea));
+            //Console.SetOut(new StreamWriter(TextArea));
+
+            // Initialize the DataGridView.
+            GridView.AutoGenerateColumns = false;
+            GridView.AutoSize = true;
+            //dataGridView1.DataSource = BSource;
+
+            BSource.Add("Bob");
+            GridView.DataSource = BSource;
+
+            // Initialize the form.
+            AutoSize = true;
         }
 
         /// <summary>
@@ -61,23 +98,16 @@ namespace NiceMeter
         /// </summary>
         public MainWindow()
         {
-            // Disabling settings as they appear to do nothing. Pass empty
-            Settings settings = new Settings(new Dictionary<string, string>
-            {
-                //{ "/intelcpu/0/temperature/0/values", "H4sIAAAAAAAEAOy9B2AcSZYlJi9tynt/SvVK1+B0oQiAYBMk2JBAEOzBiM3mkuwdaUcjKasqgcplVmVdZhZAzO2dvPfee++999577733ujudTif33/8/XGZkAWz2zkrayZ4hgKrIHz9+fB8/Iu6//MH37x79i9/+NX6N3/TJm9/5f/01fw1+fosnv+A/+OlfS37/jZ/s/Lpv9fff6Ml/NTef/yZPnozc5679b+i193//TQZ+/w2Dd+P9/sZeX/67v/GTf/b3iP3u4/ObBL//73+i+f039+D8Zk/+xz/e/P6beu2TQZju8yH8f6OgzcvPv/U3/Rb8+z/0f/9b/+yfaOn8079X6fr6Cws7ln/iHzNwflPv99/wyS/+xY4+v/evcJ+733+jJ5//Cw7/4ndy9Im3+U2e/Fbnrk31C93vrt/fyPvdb+N//hsF7/4/AQAA//9NLZZ8WAIAAA==" },
-                //{ "/intelcpu/0/load/0/values", "H4sIAAAAAAAEAOy9B2AcSZYlJi9tynt/SvVK1+B0oQiAYBMk2JBAEOzBiM3mkuwdaUcjKasqgcplVmVdZhZAzO2dvPfee++999577733ujudTif33/8/XGZkAWz2zkrayZ4hgKrIHz9+fB8/Iu6//MH37x79i9++mpwcv/md/9df89egZ/xX/ym/5y/4D37618Lv7ya//u+58+u+5d9/z7/5t/w9/6u5fP5bH/6av+eTkXyefXxp26ONaf/v/dG/sf39D/rvnv4e5vc/0IP56/waK/vuHzf5I38P8/tv+mv8Rbb9f0pwTF9/zr/1X9vP/8I//+/6Pf7Z30N+/zdf/HX29zd/859q4aCNP5b//U+U3/+7f+zXOjZwfqvDX/V7/o9/vPz+a1G/pv0f+fGlhfk7eZ//N3/0v28//5X0u/n8Cxq7+f1X/tHft20A5x8a/W5/02+BP36Nf+j/nv8XfzrT+c2//Ob4p3+vktvUhNs/+xcWikP6e/4T/5jS5M8/sL8vP/5ff49f/Ivl9//sHzv6PX/vXyG//9R/94/9HuZ34P/5vyC//3W/5e/1exa/k+Bw4bUBnU2bP4Xg/1bn0uafeTH6PatfKL//N3/0t2y/gG9+/8+IzqYNxmU+/+jwX7afY67/nwAAAP//GYSA31gCAAA=" },
-            });
-
-            Computer = new Computer(settings) {
-                CPUEnabled = true,
-                FanControllerEnabled = true
-            };
-
             Computer.Open();
 
             InitializeComponent();
 
-            Timer.Tick += new EventHandler(TimerTick);
+            //Timer.Tick += new EventHandler(TimerTick);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
