8ddbabab20373b8879aff226be7fa1be1061dd92
micheled <mail@thisisdurante.com>
2018-11-01 15:46:44 +0100

    Minor amend

--- NiceMeter/ViewModels/CpuMeter.cs
+++ NiceMeter/ViewModels/CpuMeter.cs
@@ -8,6 +8,7 @@ namespace NiceMeter.ViewModels
     class CpuMeter : AbstractMeter, IMeter
     {
         private Unit power = null;
+        private Unit load = null;
 
         public CpuMeter(string name)
         {
@@ -18,19 +19,21 @@ namespace NiceMeter.ViewModels
         public IMeter FilterSensors(IList<ISensor> sensors)
         {
             power = new Unit(sensors.Where(x => x.SensorType == SensorType.Power && x.Name.Contains("CPU Package")).First().Value, "W", "{0:N0}");
+            load = new Unit(sensors.Where(x => x.SensorType == SensorType.Load && x.Name.Contains("CPU Total")).First().Value, "%", "{0:N0}");
             return this;
         }
 
         public IMeter GetDisplayMeter()
         {
-            Text = power.ToString();
+            Text = string.Format("{0}, {1}", load.ToString(), power.ToString());
             return this;
         }
 
         public void UpdateMeter(IList<ISensor> sensors)
         {
             power.Value = sensors.Where(x => x.SensorType == SensorType.Power && x.Name.Contains("CPU Package")).First().Value;
-            Text = power.ToString();
+            load.Value = sensors.Where(x => x.SensorType == SensorType.Load && x.Name.Contains("CPU Total")).First().Value;
+            Text = string.Format("{0}, {1}", load.ToString(), power.ToString());
         }
     }
 }
